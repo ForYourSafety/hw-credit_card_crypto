02d8e00f263b66a9540b299ed6073c472d8429f6
Yi Kuo <kuokuoyiyi@gmail.com>
2025-03-13 21:03:00 +0800

    tests(crypto): add more keys for testing

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -9,7 +9,7 @@ describe 'Test card info encryption' do
   before do
     @cc = CreditCard.new('4916603231464963', 'Mar-30-2020',
                          'Soumya Ray', 'Visa')
-    @key = 3
+    @keys = [3, 100, 114_514, 1_919_810, 1_145_141_919_810, 2_147_483_647]
   end
 
   ciphers = [SubstitutionCipher::Caesar, SubstitutionCipher::Permutation, DoubleTranspositionCipher]
@@ -17,15 +17,19 @@ describe 'Test card info encryption' do
   ciphers.each do |cipher|
     describe "Using #{cipher}" do
       it 'should encrypt card information' do
-        enc = cipher.encrypt(@cc.to_s, @key)
-        _(enc).wont_equal @cc.to_s
-        _(enc).wont_be_nil
+        @keys.each do |key|
+          enc = cipher.encrypt(@cc.to_s, key)
+          _(enc).wont_equal @cc.to_s
+          _(enc).wont_be_nil
+        end
       end
 
       it 'should decrypt text' do
-        enc = cipher.encrypt(@cc.to_s, @key)
-        dec = cipher.decrypt(enc, @key)
-        _(dec).must_equal @cc.to_s
+        @keys.each do |key|
+          enc = cipher.encrypt(@cc.to_s, key)
+          dec = cipher.decrypt(enc, key)
+          _(dec).must_equal @cc.to_s
+        end
       end
     end
   end
