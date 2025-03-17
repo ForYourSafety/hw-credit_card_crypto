83d02c37660a421d1f5d2ea26d1b81f5c6e2d8a3
Yi Kuo <kuokuoyiyi@gmail.com>
2025-03-17 20:30:17 +0800

    tests(hash): add hash tests

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -26,10 +26,24 @@ end
 
 describe 'Test hashing requirements' do
   describe 'Check hashes are consistently produced' do
-    # TODO: Check that each card produces the same hash if hashed repeatedly
+    # Check that each card produces the same hash if hashed repeatedly
+    cards.each do |card|
+      it 'should produce the same hash if hashed repeatedly' do
+        hash1 = card.hash
+        _(hash1).wont_be_nil
+
+        hash2 = card.hash
+        _(hash2).must_equal hash1
+      end
+    end
   end
 
   describe 'Check for unique hashes' do
-    # TODO: Check that each card produces a different hash than other cards
+    # Check that each card produces a different hash than other cards
+    cards.combination(2).each do |card1, card2|
+      it 'should produce a different hash than other cards' do
+        _(card1.hash).wont_equal card2.hash
+      end
+    end
   end
 end
